58b33a5eb1094cd032d214e5b3c4aced2ec8e66f
Duy Nguyen <duynguyen@myconstant.com>
2022-06-20 09:26:53 +0700

    update

--- near/nft-lend/src/sale.rs
+++ near/nft-lend/src/sale.rs
@@ -62,6 +62,170 @@ pub struct PurchaseArgs {
 
 #[near_bindgen]
 impl Contract {
+    #[payable]
+    pub fn offer_by_near(&mut self, msg: String) {
+        let PurchaseArgs {
+            nft_contract_id,
+            token_id,
+            action,
+            loan_principal_amount,
+            loan_duration,
+            loan_interest_rate,
+            available_at,
+        } = near_sdk::serde_json::from_str(&msg).expect("Invalid PurchaseArgs");
+
+        let sender_id = env::predecessor_account_id();
+        let contract_and_token_id = format!("{}{}{}", nft_contract_id, DELIMETER, token_id);
+        let mut sale = self
+            .sales
+            .get(&contract_and_token_id)
+            .expect("No sale in ft_on_transfer");
+
+        let amount = env::attached_deposit();
+        assert!(amount > 0, "Amount must be greater than 0");
+        assert_ne!(sale.owner_id, sender_id, "Cannot buy your own sale.");
+
+        if action == "offer_now" {
+            assert_ne!(sale.owner_id, sender_id, "Cannot buy your own sale.");
+            let log_message = format!(
+                "Principle amount {}, real amount {}",
+                sale.loan_principal_amount, amount
+            );
+            env::log(log_message.as_bytes());
+            assert!(
+                amount == sale.loan_principal_amount,
+                "Amount must equals loan principal amount ",
+            );
+            sale.lender = sender_id;
+            sale.status = LoanStatus::Processing as u32;
+            sale.updated_at = U64(env::block_timestamp() / 1000000000);
+            sale.started_at = U64(env::block_timestamp() / 1000000000);
+            let new_offer = Offer {
+                offer_id: sale.offers.len() as u32 + 1,
+                lender_id: sale.lender.clone(),
+                loan_principal_amount: sale.loan_principal_amount,
+                loan_duration: sale.loan_duration,
+                loan_interest_rate: sale.loan_interest_rate,
+                created_at: U64(env::block_timestamp() / 1000000000),
+                updated_at: U64(env::block_timestamp() / 1000000000),
+                started_at: U64(env::block_timestamp() / 1000000000),
+                status: LoanStatus::Processing as u32,
+                available_at: available_at,
+            };
+            sale.offers.push(new_offer);
+            self.sales.insert(&contract_and_token_id, &sale);
+            ext_contract::nft_transfer(
+                env::current_account_id(),
+                token_id,
+                sale.approval_id,
+                "payout from market".to_string(),
+                &nft_contract_id,
+                1,
+                GAS_FOR_NFT_TRANSFER,
+            );
+            //
+        } else if action == "offer" {
+            assert_ne!(sale.owner_id, sender_id, "Cannot buy your own sale.");
+            sale.lender = sender_id;
+            //insert offer
+            let new_offer = Offer {
+                offer_id: sale.offers.len() as u32 + 1,
+                lender_id: sale.lender.clone(),
+                loan_principal_amount: amount,
+                loan_duration: loan_duration,
+                loan_interest_rate: loan_interest_rate,
+                created_at: U64(env::block_timestamp() / 1000000000),
+                updated_at: U64(env::block_timestamp() / 1000000000),
+                started_at: U64(0),
+                status: LoanStatus::Open as u32,
+                available_at: available_at,
+            };
+            sale.offers.push(new_offer);
+            //
+            self.sales.insert(&contract_and_token_id, &sale);
+            //
+        }
+    }
+
+    #[payable]
+    pub fn pay_back_loan_by_near(&mut self, nft_contract_id: ValidAccountId, token_id: String) {
+        let contract_and_token_id = format!("{}{}{}", nft_contract_id, DELIMETER, token_id);
+        let mut sale = self
+            .sales
+            .get(&contract_and_token_id)
+            .expect("No sale in ft_on_transfer");
+
+        let amount = env::attached_deposit();
+        assert!(
+            amount > sale.loan_principal_amount,
+            "Amount must greater than loan principal amount ",
+        );
+        sale.status = LoanStatus::Done as u32;
+        sale.updated_at = U64(env::block_timestamp() / 1000000000);
+        self.sales.insert(&contract_and_token_id, &sale);
+        let contract_and_token_id = format!("{}{}{}", nft_contract_id, DELIMETER, token_id.clone());
+        let sale = self.sales.get(&contract_and_token_id).expect("No sale");
+        let now: u128 = (env::block_timestamp() / 1000000000) as u128;
+        let expired = sale.started_at.0 as u128 + sale.loan_duration as u128;
+        assert!(expired > now, "invalid time to pay back loan ");
+        let real_pay_amount = self.calculate_pay_amount(
+            sale.loan_principal_amount,
+            sale.loan_duration,
+            sale.loan_interest_rate,
+            sale.started_at.0 as u128,
+            (env::block_timestamp() / 1000000000) as u128,
+        );
+        assert!(
+            real_pay_amount == amount,
+            "invalid payment amount, pay_amount:{}, input_amount{}",
+            real_pay_amount,
+            amount
+        );
+        let mut offers: Vec<Offer> = Vec::new();
+        for v in sale.offers {
+            if v.status == LoanStatus::Processing as u32 {
+                let mut clone = self.clone_offer(v);
+                clone.status = LoanStatus::Done as u32;
+                clone.updated_at = U64(env::block_timestamp() / 1000000000);
+                offers.push(clone);
+            } else {
+                let clone = self.clone_offer(v);
+                offers.push(clone);
+            }
+        }
+        self.sales.insert(
+            &contract_and_token_id,
+            &Sale {
+                owner_id: sale.owner_id.clone().into(),
+                approval_id: sale.approval_id,
+                nft_contract_id: sale.nft_contract_id.clone(),
+                token_id: sale.token_id.clone(),
+                loan_principal_amount: sale.loan_principal_amount,
+                loan_duration: sale.loan_duration,
+                loan_currency: sale.loan_currency,
+                loan_interest_rate: sale.loan_interest_rate,
+                loan_config: sale.loan_config,
+                available_at: sale.available_at,
+                created_at: sale.created_at,
+                updated_at: U64(env::block_timestamp() / 1000000000),
+                started_at: sale.started_at,
+                status: sale.status,
+                lender: sale.lender.clone(),
+                offers: offers,
+            },
+        );
+        Promise::new(sale.lender).transfer(amount);
+        ext_contract::nft_transfer(
+            sale.owner_id,
+            token_id,
+            0,
+            "payout from market".to_string(),
+            &nft_contract_id,
+            1,
+            GAS_FOR_NFT_TRANSFER,
+        );
+    }
+
     /// for add sale see: nft_callbacks.rs
     #[payable]
     pub fn cancel_loan(&mut self, nft_contract_id: ValidAccountId, token_id: String) {
@@ -83,14 +247,18 @@ impl Contract {
             let mut offers: Vec<Offer> = Vec::new();
             for v in sale.offers {
                 if v.status == LoanStatus::Open as u32 {
-                    ext_contract::ft_transfer(
-                        v.lender_id.clone(),
-                        U128(v.loan_principal_amount),
-                        Some(String::from("refund from market")),
-                        &sale.loan_currency,
-                        1,
-                        GAS_FOR_FT_TRANSFER,
-                    );
+                    if sale.loan_currency == "near" {
+                        Promise::new(v.lender_id.clone()).transfer(U128(v.loan_principal_amount).0);
+                    } else {
+                        ext_contract::ft_transfer(
+                            v.lender_id.clone(),
+                            U128(v.loan_principal_amount),
+                            Some(String::from("refund from market")),
+                            &sale.loan_currency,
+                            1,
+                            GAS_FOR_FT_TRANSFER,
+                        );
+                    }
                     let mut clone = self.clone_offer(v);
                     clone.status = LoanStatus::Refunded as u32;
                     clone.updated_at = U64(env::block_timestamp() / 1000000000);
@@ -133,14 +301,18 @@ impl Contract {
                     v.status == LoanStatus::Open as u32,
                     "Unable cancel 'not open' offer "
                 );
-                ext_contract::ft_transfer(
-                    v.lender_id.clone(),
-                    U128(v.loan_principal_amount),
-                    Some(String::from("refund from market")),
-                    &sale.loan_currency,
-                    1,
-                    GAS_FOR_FT_TRANSFER,
-                );
+                if sale.loan_currency == "near" {
+                    Promise::new(v.lender_id.clone()).transfer(U128(v.loan_principal_amount).0);
+                } else {
+                    ext_contract::ft_transfer(
+                        v.lender_id.clone(),
+                        U128(v.loan_principal_amount),
+                        Some(String::from("refund from market")),
+                        &sale.loan_currency,
+                        1,
+                        GAS_FOR_FT_TRANSFER,
+                    );
+                }
                 let mut clone = self.clone_offer(v);
                 clone.status = LoanStatus::Refunded as u32;
                 clone.updated_at = U64(env::block_timestamp() / 1000000000);
@@ -231,6 +403,7 @@ impl Contract {
             sale.status == LoanStatus::Open as u32,
             "Loan is in proccessing "
         );
+        let loan_currency = sale.loan_currency.clone();
         let mut offers: Vec<Offer> = Vec::new();
         let mut updated_sale = Sale {
             owner_id: sale.owner_id.clone().into(),
@@ -254,15 +427,27 @@ impl Contract {
             let mut clone = self.clone_offer(v);
             clone.updated_at = U64(env::block_timestamp() / 1000000000);
             if clone.offer_id == offer_id && clone.status == LoanStatus::Open as u32 {
-                self.process_purchase(
-                    contract_id.clone(),
-                    token_id.clone(),
-                    updated_sale.loan_currency.clone(),
-                    U128(clone.loan_principal_amount),
-                    sale.approval_id,
-                    sale.owner_id.clone(),
-                    sale.lender.clone(),
-                );
+                if loan_currency == "near" {
+                    ext_contract::nft_transfer(
+                        env::current_account_id(),
+                        token_id.clone(),
+                        sale.approval_id,
+                        "payout from market".to_string(),
+                        &contract_id,
+                        1,
+                        GAS_FOR_NFT_TRANSFER,
+                    );
+                } else {
+                    self.process_purchase(
+                        contract_id.clone(),
+                        token_id.clone(),
+                        updated_sale.loan_currency.clone(),
+                        U128(clone.loan_principal_amount),
+                        sale.approval_id,
+                        sale.owner_id.clone(),
+                        sale.lender.clone(),
+                    );
+                }
                 clone.status = LoanStatus::Processing as u32;
                 clone.started_at = U64(env::block_timestamp() / 1000000000);
                 updated_sale.loan_duration = clone.loan_duration;
@@ -270,14 +455,19 @@ impl Contract {
                 updated_sale.loan_interest_rate = clone.loan_interest_rate;
             } else {
                 if clone.status == LoanStatus::Open as u32 {
-                    ext_contract::ft_transfer(
-                        clone.lender_id.clone(),
-                        U128(clone.loan_principal_amount.clone()),
-                        Some(String::from("refund from market")),
-                        &updated_sale.loan_currency,
-                        1,
-                        GAS_FOR_FT_TRANSFER,
-                    );
+                    if loan_currency == "near" {
+                        Promise::new(clone.lender_id.clone())
+                            .transfer(U128(clone.loan_principal_amount.clone()).0);
+                    } else {
+                        ext_contract::ft_transfer(
+                            clone.lender_id.clone(),
+                            U128(clone.loan_principal_amount.clone()),
+                            Some(String::from("refund from market")),
+                            &updated_sale.loan_currency,
+                            1,
+                            GAS_FOR_FT_TRANSFER,
+                        );
+                    }
                     clone.status = LoanStatus::Refunded as u32;
                 }
             }
@@ -474,9 +664,7 @@ impl Contract {
         )
     }
 
-    /// self callback
-
-    #[private]
+    // #[private]
     pub fn resolve_purchase(
         &mut self,
         ft_token_id: AccountId,
