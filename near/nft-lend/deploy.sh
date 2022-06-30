8b4208baaf729eb94fd03540449ad9c710bac102
Duy Nguyen <duynguyen@myconstant.com>
2022-06-30 10:47:07 +0700

    update

--- near/nft-lend/deploy.sh
+++ near/nft-lend/deploy.sh
@@ -1,8 +1,8 @@
 git reset --hard
 git checkout master
 git pull
-export NEAR_ENV=$1
+export NEAR_ENV=mainnet
 near login
 
-PAWN_ID=$2
+PAWN_ID=nftpawn-protocol.near
 ./build.sh && near deploy --wasmFile target/wasm32-unknown-unknown/release/nft_pawn.wasm --accountId $PAWN_ID
--- near/nft-lend/deploy_testnet.sh
+++ near/nft-lend/deploy_testnet.sh
@@ -1,4 +1,5 @@
 export NEAR_ENV=testnet
 dev=pawn-dev1.testnet
-PAWN_ID=pawn-dev1.testnet
+PAWN_ID=duynguyen-pawn9.testnet
 ./build.sh && near deploy --wasmFile target/wasm32-unknown-unknown/release/nft_pawn.wasm --accountId $PAWN_ID
+./build.sh && near deploy --wasmFile target/wasm32-unknown-unknown/release/nft_pawn.wasm --accountId $dev
